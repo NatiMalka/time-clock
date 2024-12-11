1064387eb0264a4bdcc8fb186ae10081acf1b4e5
netanel <netanel.malka@tomax.io>
2024-12-11 17:58:16 +0200

    Updates

--- src/components/SomeComponent.tsx
+++
@@ -1,8 +0,0 @@
-import { Fingerprint, Key } from 'lucide-react';
-import { Icon } from './ui/Icon';
-
-// Instead of:
-// <Fingerprint className="h-6 w-6" />
-
-// Use:
-<Icon icon={Fingerprint} fallback={Key} className="h-6 w-6" /> 
